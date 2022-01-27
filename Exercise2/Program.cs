dd06447914c875e2a1659269817279b201bee233
Konrad Knauer <konrad.knauer@tobit.com>
2022-01-27 14:42:12 +0100

    :sparkles: Add Exercise2.csproj

---
+++ Exercise2/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * Next please convert the property pattern below into an extended property pattern.
+ */
+// ToDo: Add nested models and property pattern
+
+Console.WriteLine("Hello, World!");
