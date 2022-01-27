2a83b4cf50ba8387ff171071e30abc0254a624ae
Konrad Knauer <konrad.knauer@tobit.com>
2022-01-27 12:34:00 +0100

    :sparkles: Add initial files

---
+++ ANWExample/Models/Test.cs
@@ -0,0 +1,17 @@
+﻿namespace ANWExample.Models;
+
+/// <summary>
+/// A test
+/// </summary>
+public record Test
+{
+    /// <summary>
+    /// The tested user
+    /// </summary>
+    public Person User { get; set; }
+
+    /// <summary>
+    /// The grade
+    /// </summary>
+    public int Grade { get; set; }
+}
---
+++ ANWExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using ANWExample.Models;
+
+var test = new Test
+{
+    User = new Person(
+        "Max",
+        "Mustermann",
+        new DateTimeOffset(
+            2000,
+            1,
+            1,
+            1,
+            1,
+            1,
+            new TimeSpan(0, 1, 0, 0)),
+        Gender.Unknown,
+        new Address
+        {
+            City = "Musterhausen",
+            Street = "Musterstraße 666",
+            Zip = "66666"
+        })
+};
+
+Console.WriteLine(test);
+Console.WriteLine(test is { User.Address.Zip: "66666" });
