9ae5a3bc7854821352cd7a84377982e2bd16c52b
Konrad Knauer <konrad.knauer@tobit.com>
2022-01-27 14:11:46 +0100

    🚧 Add ToDo

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -2,5 +2,5 @@
  * After you now have seen the new way of creating the main method now
  * your task is to update this file to use the new features.
  */
-
+//ToDo: Add old Program.cs
 Console.WriteLine("Hello, World!");
