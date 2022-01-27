19bd4d27e9e3aecc8e4958f39066712af1408973
Konrad Knauer <konrad.knauer@tobit.com>
2022-01-27 14:33:05 +0100

    :sparkles: Add first exercise

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,6 +1,24 @@
 ﻿/*
  * After you now have seen the new way of creating the main method now
  * your task is to update this file to use the new features.
+ *
+ * Note: The xml-documentation is optional
  */
-//ToDo: Add old Program.cs
-Console.WriteLine("Hello, World!");
+
+namespace Exercise1
+{
+    /// <summary>
+    /// The default program
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application
+        /// </summary>
+        /// <returns></returns>
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
