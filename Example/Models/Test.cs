3fc9a6ae8b0cd336ab2b8c941c14ec820ca588bb
Kamau <k.knauer97@gmail.com>
2022-01-30 13:46:09 +0100

    :art: Reformat code

--- Example/Models/Test.cs
+++ Example/Models/Test.cs
@@ -5,13 +5,13 @@
 /// </summary>
 public record Test
 {
-    /// <summary>
-    /// The tested user
-    /// </summary>
-    public Person User { get; set; }
+	/// <summary>
+	/// The tested user
+	/// </summary>
+	public Person User { get; set; }
 
-    /// <summary>
-    /// The grade
-    /// </summary>
-    public int Grade { get; set; }
+	/// <summary>
+	/// The grade
+	/// </summary>
+	public int Grade { get; set; }
 }
--- Example/Program.cs
+++ Example/Program.cs
@@ -8,24 +8,24 @@
 
 var test = new Test
 {
-    User = new Person(
-        "Max",
-        "Mustermann",
-        new DateTimeOffset(
-            2000,
-            1,
-            1,
-            1,
-            1,
-            1,
-            new TimeSpan(0, 1, 0, 0)),
-        Gender.Unknown,
-        new Address
-        {
-            City = "Musterhausen",
-            Street = "Musterstraße 666",
-            Zip = "66666"
-        })
+	User = new Person(
+		"Max",
+		"Mustermann",
+		new DateTimeOffset(
+			2000,
+			1,
+			1,
+			1,
+			1,
+			1,
+			new TimeSpan(0, 1, 0, 0)),
+		Gender.Unknown,
+		new Address
+		{
+			City = "Musterhausen",
+			Street = "Musterstraße 666",
+			Zip = "66666"
+		})
 };
 
 Console.WriteLine(test);
