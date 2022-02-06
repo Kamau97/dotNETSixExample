163d638bb4e141fad146f6e66e534d1763a0cdea
Kamau <k.knauer97@gmail.com>
2022-02-06 11:28:10 +0100

    :sparkles: Add third exercise

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-// ToDo: Add task for "build an interpolated string handler"
+﻿// 1. Build an interpolated string handler for a case of your choice.
+//		As a reference you can look at https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/tutorials/interpolated-string-handler
+// 2. After you've build the handler use it to create a constant interpolated string and log it in the console.
+// 3. Now create a record with at least two properties
+//		3.1. Create a ToString() on the record
+//		3.2. Seal the ToString() of the record
+//		3.3. Create a new record inheriting from your record from before
+//			3.3.1. Log the ToString() output of a instance of your record in the console
+//			3.3.2. Deconstruct an instance of your record in at least one existing and one newly defined variable
+
 Console.WriteLine("Hello, World!");
